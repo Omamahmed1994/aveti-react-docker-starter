4ce1b4e8051f0ba93d324c054c00cebf9cc19308
aveti_to <niranjan.msit1@gmail.com>
2020-07-27 07:20:57 -0700

    Docker volume change

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Docker Volumes.</p>
+        <p>Thanks for watching</p>
         <a
           className="App-link"
           href="https://www.youtube.com/channel/UCWR5dG9sg5du-Zhq23m9kYA"
